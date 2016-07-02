d63e2b50ea7fa34190f87b72aa0f15042ef61248
ipavlenko <ipavlenko@inbox.ru>
2016-07-02 18:50:14 +0500

    Structure update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,106 @@ var Landing =
 
     Landing = {};
 
+    $(document).ready(function() {
+
+        $('[data-vue-app]').each(function(index, element) {
+
+            var data = $(element).data();
+
+            var App = Vue.extend({
+                data: function() {
+                    return data;
+                },
+                created: function() {
+
+                    Vue.service('security', Core.SecurityFactory(this));
+                    Vue.service('portals', Core.PortalsFactory(this));
+                },
+            });
+
+            var router = new VueRouter({
+                history: true,
+            });
+
+            router.beforeEach(function(transition) {
+
+                if (transition.to.auth && !router.app.principal) {
+                    transition.abort();
+                } else if (transition.to.anon && router.app.principal) {
+                    transition.abort();
+                } else {
+                    transition.next();
+                }
+            });
+
+            var routes = {
+                '/': {
+                    component: Landing.LandingPage,
+                },
+                '/gallery': {
+                    component: Landing.LandingGalleryPage,
+                },
+                '/storage': {
+                    component: Landing.LandingStoragePage,
+                },
+                '/signin': {
+                    component: Landing.LandingSigninPage,
+                    anon: true,
+                },
+                '/signup': {
+                    component: Landing.LandingSignupPage,
+                    anon: true,
+                },
+                '/manage': {
+                    component: Landing.LandingManagePage,
+                    auth: true,
+                },
+                '/manage-create': {
+                    component: Landing.LandingManageCreatePage,
+                    auth: true,
+                },
+                '/site/:portal/:page': {
+                    component: Shell.ShellPublic,
+                    auth: true,
+                },
+                '/manage/:portal': {
+                    component: Shell.Loader,
+                    auth: true,
+                    private: true,
+                },
+                '/manage/:portal/:page': {
+                    component: Shell.Loader,
+                    auth: true,
+                    private: true,
+                },
+            };
+
+            function createRoute(page) {
+                return {
+                    component: Shell.ShellPublic.extend({
+                        data: function() {
+                            return {
+                                page: page,
+                            };
+                        }
+                    }),
+                };
+            }
+
+            if (data.model) {
+                for (var i = 0; i < data.model.pages.length; i++) {
+
+                    var page = data.model.pages[i];
+                    routes[page.name] = createRoute(page);
+                }
+            }
+
+            router.map(routes);
+
+            router.start(App, $('[data-vue-body]', element).get(0));
+        });
+    });
+
     return Landing;
 
 })(jQuery, Vue, Core, Shell);
